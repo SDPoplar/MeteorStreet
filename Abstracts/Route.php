309abc12c6709554f0ec175c220e5acafbe544ab
SDPoplar <741027037@qq.com>
2021-10-15 17:24:04 +0800

    reformat some code to match psr-0

--- Abstracts/Route.php
+++ Abstracts/Route.php
@@ -2,24 +2,24 @@
 namespace Mxs\Abstracts;
 
 class Route {
-    public function match( \Mxs\Bases\Request $request ) : ?RouteRule {
-        foreach( $this->_rules as $rule ) {
-            if( $rule->matched( $request ) ) {
+    public function match(\Mxs\Bases\Request $request) : ?RouteRule {
+        foreach ($this->_rules as $rule) {
+            if ($rule->matched($request)) {
                 return $rule;
             }
         }
         return null;
     }
 
-    protected function &appendRule( \Mxs\Abstracts\RouteRule $rule ) : Route {
-        array_push( $this->_rules, $rule );
+    protected function &appendRule(\Mxs\Abstracts\RouteRule $rule) : Route {
+        array_push($this->_rules, $rule);
         return $this;
     }
 
     protected function &mergeRule( array $rules ) : Route {
-        foreach( $rules as $rule ) {
-            if( is_subclass_of( $rule, \Mxs\Abstracts\RouteRule::class ) ) {
-                $this->appendRule( $rule );
+        foreach ($rules as $rule) {
+            if (is_subclass_of($rule, \Mxs\Abstracts\RouteRule::class)) {
+                $this->appendRule($rule);
             }
         }
         return $this;
