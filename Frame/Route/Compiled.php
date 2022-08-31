ea5c99748a0e7b41f4c8a961a18629c092dfd43e
SDPoplar <741027037@qq.com>
2022-08-31 17:22:20 +0800

    update route finder

--- Frame/Route/Compiled.php
+++ Frame/Route/Compiled.php
@@ -7,26 +7,8 @@ class Compiled
 {
     public function search(string $url): Item
     {
-        $parts = array_filter(explode('/', trim($url)));
-        if (empty($parts)) {
-            $from = $this->all_path['*'];
-        } else {
-            $from = $this->all_path;
-            while (!empty($parts)) {
-                $part = array_shift($parts);
-                if (array_key_exists($part, $from)) {
-                    $from = $from[$part];
-                    continue;
-                }
-    
-                if (array_key_exists('_pattern', $from)) {
-                    $params[] = $part;
-                    $from = $from['_pattern'];
-                    continue;
-                }
-                $from = null;
-            }
-        }
+        $params = [];
+        $from = self::pickItem($this->all_path, array_filter(explode('/', trim($url))), $params);
         $from or (new \Mxs\Exceptions\Runtimes\RouteNotFound($this->method->value, $url))->occur();
         return (new Item(...$from))->withRouteParams($params ?? []);
     }
@@ -35,13 +17,30 @@ class Compiled
     {
         $emethod = is_string($method) ? HttpMethods::tryFrom(strtoupper($method)) : $method;
         $emethod or (new \Mxs\Exceptions\Runtimes\UnkownHttpMethod($method))->occur();
-        $all = \Mxs\Frame\FileStructure::get()->getCompiledPath('routes'.strtolower($emethod->value).'.php');
+        $all = \Mxs\Frame\FileStructure::get()->getCompiledPath('routes/'.strtolower($emethod->value).'.php');
         return new self($all, $emethod);
     }
 
     protected function __construct(string $file_path, public readonly HttpMethods $method)
     {
         $this->all_path = is_readable($file_path) ? (include($file_path) ?: []) : [];
+    }
+
+    protected static function pickItem(array $all, array $parts, array &$route_params): ?array
+    {
+        do {
+            $search = array_shift($parts);
+            if (empty($search)) {
+                return $all['index'];
+            }
+            if (array_key_exists($search, $all['items'])) {
+                $all = $all['items'][$search];
+            } else {
+                $all = $all['patterns'];
+                $route_params[] = $search;
+            }
+        } while (!empty($all));
+        return null;
     }
 
     protected readonly array $all_path;
