3b40930ec5d2a0e0443d3ebb7f11019f7b790ad1
SDPoplar <741027037@qq.com>
2026-05-25 16:50:03 +0000

    add del() to redisfacade interface

--- src/Services/Redis/RedisFacadeInterface.php
+++ src/Services/Redis/RedisFacadeInterface.php
@@ -8,4 +8,5 @@ interface RedisFacadeInterface
 {
     public function get(string $key): mixed;
     public function set(string $key, mixed $value, mixed $options = null): \Redis|string|bool;
+    public function del(array|string $key, string ...$other_keys): Redis|int|false;
 }
